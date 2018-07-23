f47a8767896a45a2371087e6af25df48e9ea3b31
danielfromemarsys <41156875+danielfromemarsys@users.noreply.github.com>
2018-07-23 14:07:48 +0200

    Update power.go
    adds possibility to charge gergo's laptop

--- power.go
+++ power.go
@@ -6,14 +6,19 @@ import (
 )
 
 func main() {
-    dani := flag.Int("dani", 0, "Dani laptopjanak toltottsegi szintje")
-    gellert := flag.Int("gellert", 0, "Gellert laptopjanak toltottsegi szintje")
+    dani := flag.Int("dani", 100, "Dani laptopjanak toltottsegi szintje")
+    gellert := flag.Int("gellert", 100, "Gellert laptopjanak toltottsegi szintje")
+    gergo := flag.Int("gellert", 100, "Gergo laptopjanak toltottsegi szintje")
+
 
     flag.Parse()
-
-    if (*dani > *gellert) {
-        fmt.Println("Gellert laptopjat kell tolteni")
-    } else {
-        fmt.Println("Dani laptopjat kell tolteni")
+    
+    switch {
+        *dani < *gellert && *dani < *gergo:
+            fmt.Println("Dani laptopjat kell tolteni")
+        *gellert < *dani && *gellert < *gergo:
+            fmt.Println("Gellert laptopjat kell tolteni")
+        default:
+            fmt.Println("Geri laptopjat kell tolteni")     
     }
 }
