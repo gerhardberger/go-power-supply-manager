20b01c127b059a220e5c228de0ccbfde8fcd8cac
danielfromemarsys <41156875+danielfromemarsys@users.noreply.github.com>
2018-10-01 11:32:41 +0200

    Update power.go

--- power.go
+++ power.go
@@ -7,17 +7,17 @@ import (
 
 func main() {
     dani := flag.Int("dani", 100, "Dani laptopjanak toltottsegi szintje")
-    gellert := flag.Int("gellert", 100, "Gellert laptopjanak toltottsegi szintje")
+    gabor := flag.Int("gabor", 100, "Gabor laptopjanak toltottsegi szintje")
     gergo := flag.Int("gergo", 100, "Gergo laptopjanak toltottsegi szintje")
 
 
     flag.Parse()
     
     switch {
-        case *dani < *gellert && *dani < *gergo:
+        case *dani < *gabor && *dani < *gergo:
             fmt.Println("Dani laptopjat kell tolteni")
-        case *gellert < *dani && *gellert < *gergo:
-            fmt.Println("Gellert laptopjat kell tolteni")
+        case *gabor < *dani && *gabor < *gergo:
+            fmt.Println("Gabor laptopjat kell tolteni")
         default:
             fmt.Println("Geri laptopjat kell tolteni")     
     }
